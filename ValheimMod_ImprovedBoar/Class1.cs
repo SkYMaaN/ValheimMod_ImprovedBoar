9212992b1dfd01d095ee48cfaab742538243a940
Alexandr <sashakosovan4@gmail.com>
2021-04-13 21:45:00 +0300

    Маленькие кабаны теперь не боятся огня Кабаны теперь могут разможаться и кушать в зоне горенияДобавьте файлы проекта.

---
+++ ValheimMod_ImprovedBoar/Class1.cs
@@ -0,0 +1,64 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using BepInEx.Configuration;
+
+namespace ValheimMod
+{
+    [BepInPlugin("SkYMaN.ValheimMod_ImprovedBoar", "ImprovedBoar", "1.0.2")]
+    [BepInProcess("valheim.exe")]
+    public class Valheim_TeleportActivationDistanceClass : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("SkYMaN.ValheimMod_ImprovedBoar");
+        private static ConfigEntry<bool> configEntry_CommandableBoar;
+        private static ConfigEntry<bool> configEntry_AfraidFireBoar;
+        private static ConfigEntry<bool> configEntry_MonsterFaction;
+        void Awake()
+        {
+            Debug.Log("Starting loading mod - ImprovedBoar");
+            harmony.PatchAll();
+            configEntry_CommandableBoar = Config.Bind<bool>("General", "Commandable Boar", false, "Commandable Boar");
+            configEntry_AfraidFireBoar = Config.Bind<bool>("General", "Boar Afraid Fire", true, "Boar Afraid Fire");
+            configEntry_MonsterFaction = Config.Bind<bool>("General", "Monster Faction Boar", false, "Monster Faction Boar");
+            Debug.Log("Finish loading mod - ImprovedBoar");
+        }
+        void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+        [HarmonyPatch(typeof(Tameable), "Interact")]
+        class Patch_Boar
+        {
+            static void Prefix()
+            {
+                foreach (BaseAI obj in BaseAI.GetAllInstances())
+                {
+                    if (obj.name == "Boar(Clone)")
+                    {
+                        obj.m_avoidFire = false;
+                        obj.m_afraidOfFire = configEntry_AfraidFireBoar.Value;
+                        Tameable tamedcomponent = obj.GetComponent<Tameable>();
+                        if ((bool)tamedcomponent)
+                        {
+                            tamedcomponent.m_commandable = configEntry_CommandableBoar.Value;
+                            Character character = obj.GetComponent<Character>();
+                            if (configEntry_MonsterFaction.Value == true)
+                            {
+                                character.m_faction = Character.Faction.ForestMonsters;
+                            }
+                            else
+                            {
+                                character.m_faction = Character.Faction.AnimalsVeg;
+                            }
+                        }
+                    }
+                    else if(obj.name == "Boar_piggy(Clone)")
+                    {
+                        obj.m_avoidFire = false;
+                        obj.m_afraidOfFire = configEntry_AfraidFireBoar.Value;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ValheimMod_ImprovedBoar/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные со сборкой.
+[assembly: AssemblyTitle("ValheimMod_ImprovedBoar")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ValheimMod_ImprovedBoar")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("f75a7681-7551-4776-bdf1-ea7dfc0a6e79")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
